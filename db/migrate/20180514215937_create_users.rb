d1f4fac9e6e950d3d000bdfdda2843e9ba34319a
Alex Lien <codefellow@aqlien.com>
2018-05-15 16:42:02 -0700

    make Users active by default

--- db/migrate/20180514215937_create_users.rb
+++ db/migrate/20180514215937_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :short_name
       t.string :pronoun
-      t.boolean :active
+      t.boolean :active, default: true
       t.boolean :admin
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -19,16 +19,16 @@ ActiveRecord::Schema.define(version: 20180514221159) do
     t.string   "name"
     t.string   "short_name"
     t.string   "pronoun"
-    t.boolean  "active"
+    t.boolean  "active",                 default: true
     t.boolean  "admin"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
