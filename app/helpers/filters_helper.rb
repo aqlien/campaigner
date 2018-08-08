628926cd47119f5d711236a849d1839c946a955f
Alex Lien <codefellow@aqlien.com>
2018-08-07 17:12:56 -0700

    update filters page to better display multi-select answers

--- app/helpers/filters_helper.rb
+++ app/helpers/filters_helper.rb
@@ -1,13 +1,20 @@
 module FiltersHelper
-  def answer_text(answer, response)
-    text = answer.text
-    case text
-    when /String/i
-      response.string_value
-    when /Text/i
-      response.text_value
-    else
-      text
+  def display_answer_text(question, response_set)
+    pick_type = question.pick
+    case pick_type
+    when 'one', 'none'
+      response = response_set.responses.where(question_id: question.id).take
+      text = response.answer.text if response
+      case text
+      when /String/i
+        response.string_value
+      when /Text/i
+        response.text_value
+      else
+        text
+      end
+    when 'any'
+      response_set.responses.joins(:answer).where(question_id: question.id).pluck('answers.text').join('/')
     end
   end
 
@@ -17,7 +24,8 @@ module FiltersHelper
     when 'one'
       'select_searchable'
     when 'any'
-      'multi_select_searchable'
+      'text_searchable'
+      # 'multi_select_searchable'
     when 'none'
       'text_searchable'
     end
